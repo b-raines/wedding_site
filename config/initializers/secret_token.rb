c35c511d3441915809b13e4c159c7e0e953d8b5a
Brent Raines <bt.raines@gmail.com>
2013-09-08 09:43:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeddingSite::Application.config.secret_key_base = '1b9d1160f93d398369b4c9b6b0474118b5dad9ec045a0d76ec610a9c499ce9ba71610e89e0faec16a0b460f85b4947d4b628f34b57a341fab7ac06f08144b2e9'
