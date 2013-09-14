d587d1a106f938fb5bca10320b1aaa7f511025a0
Brent Raines <bt.raines@gmail.com>
2013-09-14 11:25:17 -0400

    remove ssl

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ WeddingSite::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  root 'static_pages#home'
+  root 'static_pages#temp'
   match '/signup', to: 'users#new', via: 'get'
   match '/signin', to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
